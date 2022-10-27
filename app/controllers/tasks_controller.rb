88203083bb5ec8f2c5a71646bebd61c18fa017c1
rika kaminoyama <richun.06@gmail.com>
2022-10-27 13:24:45 +0900

    タスク作成の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,27 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスク登録完了！"
+    else
+      render :new
+    end
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 
 end

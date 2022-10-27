f4b4ec3ab480d2c61620a003d10f72dc5fa41180
rika kaminoyama <richun.06@gmail.com>
2022-10-27 23:14:47 +0900

    ビューの日本語化設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module TaskApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+
     config.generators do |g|
       g.test_framework :rspec,
                        model_specs: true,

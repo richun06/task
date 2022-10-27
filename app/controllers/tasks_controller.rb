1b91480494d79b19f5596da8e1d0c69a37c95307
rika kaminoyama <richun.06@gmail.com>
2022-10-27 13:55:47 +0900

    削除機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,6 +34,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "削除完了"
+  end
+
   private
 
   def task_params

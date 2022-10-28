72e6bc73cdd8483d15a0b68f494aec6e3a0b9216
rika kaminoyama <richun.06@gmail.com>
2022-10-28 13:25:41 +0900

    終了期限を登録でき、一覧と詳細画面で確認できる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :deadline)
   end
 
 end

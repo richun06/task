ef44a40481e6958f69c442eb59c4315895049485
rika kaminoyama <richun.06@gmail.com>
2022-10-30 21:17:26 +0900

    検索インデックスを貼る

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_29_094047) do
+ActiveRecord::Schema.define(version: 2022_10_30_121431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_10_29_094047) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline"
     t.integer "status"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
   create_table "users", force: :cascade do |t|

ea46803ec7f06b5227d656dc1d7a42ccaf77b5eb
rika kaminoyama <richun.06@gmail.com>
2022-10-31 12:26:21 +0900

    name/emailカラムに空文字ではないこと、文字列の長さ、メールアドレスのフォーマットのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :tasks
-  validates :email, uniqueness: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, uniqueness: true, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end

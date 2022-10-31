d48cd9dbe8eb7a951c704e97dcb140f6c8175d58
rika kaminoyama <richun.06@gmail.com>
2022-10-31 12:27:49 +0900

    保存する前にメールアドレスの値を小文字に変換

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :tasks
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, uniqueness: true, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
 end

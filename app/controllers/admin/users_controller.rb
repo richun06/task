959d39caa682cf4499f567858afc078dbca935c7
rika kaminoyama <richun.06@gmail.com>
2022-11-01 13:34:51 +0900

    管理者画面でユーザの作成するためのフォームを表示

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,46 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
+  def new
+    @user = User.new
+  end
+
   def index
     @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = user.id
+      redirect_to admin_users_path
+    else
+      render :new_admin
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :new_admin
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end

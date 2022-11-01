d3cc61a8bdd5a03adaf1586aed23b0bf7c610c0b
rika kaminoyama <richun.06@gmail.com>
2022-11-01 22:47:37 +0900

    管理画面でユーザ編集時パスワードを編集できないように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :new
 end

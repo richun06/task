7b373484a46bca74b0462073d848a54beb0ea802
rika kaminoyama <richun.06@gmail.com>
2022-11-01 22:57:38 +0900

    N+1問題を回避する設定の修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.includes(:tasks)
   end
 
   def create
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
 
   def index
-    @tasks = current_user.tasks.include(:user)
+    @tasks = current_user.tasks
 
     if params[:sort_expired]
       @tasks = @tasks.order(deadline: :desc)

27ca4208d78fcde6ca9100349979c3ac0bcdea0b
rika kaminoyama <richun.06@gmail.com>
2022-11-05 19:39:50 +0900

    ラベル機能のCRUD機能準備

---
+++ app/controllers/labels_controller.rb
@@ -0,0 +1,36 @@
+class LabelsController < ApplicationController
+
+  def new
+    @label = Label.new
+  end
+
+  def index
+    @labels = Labal.all
+  end
+
+  def create
+    @label = Label.new(label_params)
+  end
+
+  def show
+    @label = Label.find(params[:id])
+  end
+
+  def edit
+    @label = Label.find(params[:id])
+  end
+
+  def update
+    @label = Label.find(params[:id])
+  end
+
+  def destroy
+    @label = Label.find(params[:id])
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
+end

7cb6c33e1a7b48632ec72586d4d57b6bc02cb811
rika kaminoyama <richun.06@gmail.com>
2022-11-05 23:27:43 +0900

    エラーページの設定/seedに初期ラベルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 User.create!(name:"kaminoyama", email:"sample@gmail.com", password: "123456", password_confirmation: "123456", admin: true)
 Task.create!(title: "タイトル", content: "内容", deadline: "2022-12-01", status: "未着手", priority: "高", user_id: 1)
+Label.create!(name: "fist_label")
